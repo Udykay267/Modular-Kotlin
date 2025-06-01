104c545634eae67beb789b992a065188e7c634c2
Uduak Attah Unanam <uduak618@gmail.com>
2025-06-01 16:33:21 +0100

    Add files via upload
    A mobile application with great versatility in the addition of features with minimum disruption.
    Maintenance is simplified, and all testing becomes straightforward due to neatly organized, modular
    pieces First of all, the app has smoother performance runs without interferences within modules.
    Last but not least, increased productivity during development, with highly adaptable final products
    that meet evolving user requirements.

---
+++ app/src/main/java/com/example/app/core/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app.core
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
